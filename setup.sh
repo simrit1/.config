84ba32e38ea775fa5ff5cae50aade5dfd7d90c71
Programmer-X31 <66458418+Programmer-X31@users.noreply.github.com>
2021-07-03 15:16:37 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,27 @@
+sudo pacman -S zsh brave xclip neovim 
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+git clone https://github.com/Programmer-X31/.config config
+cd config
+mv -r alacritty/ ~/.config/
+mv -r kitty/ ~/.config/
+mv -r nvim/ ~/.config/
+mv .zshrc ~/
+cd ..
+rm -rf config
+
+
+
+
+
